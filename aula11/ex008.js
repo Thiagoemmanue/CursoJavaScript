97b77ad52989e9f701c61dbb80d5ba74b8ce4aa1
Thiago Emmanuel <thiagooliveira2250@gmail.com>
2022-01-20 15:24:14 -0300

    Módulo D - Condições em JS

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60.1
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+//Condição simples com apenas um if
+
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+    console.log('Cuidado, dirija com segurança!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,12 @@
+var vel = 15
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+//Condição composta com if and else
+
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+    console.log('Cuidado, dirija com segurança!')
+} else if (vel <= 20){
+    console.log('Você está abaixo da velocidade da via, siga na faixa mais à direita!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
