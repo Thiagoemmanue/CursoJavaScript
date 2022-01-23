f133002f77a786d18fdde9b8cf142707c1eeec61
Thiagoemmanue <thiagooliveira2250@gmail.com>
2022-01-23 06:19:05 -0300

    Condições múltiplas e Switch case

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,12 +1,13 @@
 var agora = new Date()
-var hora = agora
+var hora = agora.getHours()
 console.log(`Em Brasília ${hora}`)
-if(hora < 12){
-    console.log('Bom dia!')
-} else if (hora <=18) {
-    console.log('Boa tarde!!!')
-} else if (hora >= 0 && hora <= 5) {
+
+if (hora >= 0 && hora <= 5) {
     console.log('Boa Madrugada!!!')
-} else {
-    console.log('Boa noite!!!')
+} else if(hora < 12){
+        console.log('Bom dia!!!')
+}       else if (hora <= 18) {
+            console.log('Boa tarde!!!')
+}           else {
+                console.log('Boa noite!!!')
 }
