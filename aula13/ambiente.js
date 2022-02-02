f77d28ad858613a40bae49a7985d8dee372a4207
Thiagoemmanue <thiagooliveira2250@gmail.com>
2022-02-02 06:24:20 -0300

    Módulo - Repetições em JS

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,5 +1,27 @@
+
+// Primeira forma de repetição
+/*
+console.log("While testing")
+console.log("While testing")
+console.log("While testing")
+console.log("While testing")
+console.log("While testing")
+console.log("While testing")
+console.log("While testing")
+console.log("While testing")
+*/
+
+
+// Repetição com teste lógico no inicio
 var c = 1
 while (c < 6) {
     console.log(`Passo ${c}`)
     c++
 }
+
+// Repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c < 6)
